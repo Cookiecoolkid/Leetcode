c511edaacee15c4da66cd8e75937352e682a1f72
Lizheng Chen <110144448+Cookiecoolkid@users.noreply.github.com>
2024-03-30 09:46:58 +0800

    Create 1.c
    OK, get stuck in NO.1......
    WHY did "for(int num : nums)" heap overflow omg......
    That is a official solution though...

---
+++ 1.c
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashtable;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end()) return {i, it->first};
+            hashtable[nums[i]] = i;
+        }
+        return {};
+    }
+};

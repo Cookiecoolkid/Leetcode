5eab7faeefe17a0f3b5325b899ec5eb6c67fde4f
Lizheng Chen <110144448+Cookiecoolkid@users.noreply.github.com>
2024-04-08 17:03:39 +0800

    Create 快慢指针找环形链表头部142.cpp

---
+++ 链表/快慢指针找环形链表头部142.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (!head) return NULL;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        for (;;) {
+            if (!fast->next || !fast->next->next) return NULL;
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) break;
+        }
+        fast = head;
+        while (slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return fast;
+    }
+};
